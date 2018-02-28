f922b921ea2297800b0250f0dd9f3107d232959c
celeron1ghz <celeron1ghz@gmail.com>
2018-02-28 06:31:26 +0000

    gsi

--- server/src/ListGomiCommand.js
+++ server/src/ListGomiCommand.js
@@ -1,20 +1,36 @@
 const aws = require('aws-sdk');
 const dynamodb = new aws.DynamoDB.DocumentClient({ convertEmptyValues: true });
+const _ = require('lodash');
 
 class ListGomiCommand {
   constructor() {
   }
 
   run() {
-    return dynamodb.scan({
-      TableName: 'gomi_tweet',
-      KeyConditionExpression: 'id >= 0',
-      //ExpressionAttributeValues: { ':id': 0 },
-      Limit: 5,
-      ScanIndexForward: false,
-      //: next,
-    }).promise().then(data => data.Items);
+    return dynamodb
+      .get({ TableName: 'gomi_sequence', Key: { key: 'gomi_tweet' } }).promise()
+      .then(data => {
+        const seq = data.Item.current_number;
+        const param = _.rangeRight(seq, seq - 10).map(i => { return { id: i } });
+
+        return dynamodb.batchGet({
+          RequestItems: { 'gomi_tweet2': { Keys: param } }
+        }).promise();
+      })
   }
+/*
+        return dynamodb.query({
+          TableName: 'gomi_tweet2',
+          KeyConditionExpression: 'id >= 0',
+          //ExpressionAttributeValues: { ':id': 0 },
+          Limit: 5,
+          ScanIndexForward: false,
+          //: next,
+        }).promise()
+      })
+      .then(data => data.Items);
+*/
+
 }
 
 module.exports = ListGomiCommand;
